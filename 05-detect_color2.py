c738efa51008fac8384557168fef0721754ab9fe
apolloo43 <151018628+dmumu1029@users.noreply.github.com>
2024-10-21 14:59:00 +0800

    Create 05-detect_color2.py

---
+++ 05-detect_color2.py
@@ -0,0 +1,49 @@
+import rclpy  
+from rclpy.node import Node  
+from sensor_msgs.msg import Image  
+from cv_bridge import CvBridge  
+import cv2 
+import numpy as np
+
+line = 240
+
+class ImageSubcriber(Node):
+    """Create an ImageSubscriber class, which is a subclass of the Node class."""
+    
+    def __init__(self):       
+        super().__init__('image_subscriber')      
+        self.subscription = self.create_subscription(Image, 'video_frames', self.listener_callback, 10)
+        self.subscription  
+        self.br = CvBridge()
+
+    def listener_callback(self, msg):
+        cv_image = self.br.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        frame_bgr = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+
+        hsv = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2HSV)
+        low_yellow = np.array([16,143,104]) 
+        high_yellow = np.array([46,249,214])
+        yellow_mask = cv2.inRange(hsv, low_yellow, high_yellow)
+        yellow = cv2.bitwise_and(frame_bgr, frame_bgr, mask = yellow_mask)
+
+        edge = cv2.Canny(yellow, 100, 200)
+        pix = np.asarray(edge)
+        get = np.where(pix[line - 1] == 255)
+        print(get[0])
+        
+        
+        cv2.imshow('Video Frame', frame_bgr)
+        cv2.imshow('Yellow Mask', yellow_mask)               
+        cv2.waitKey(1)       
+        # self.get_logger().info('Received video frame....')
+
+def main(args=None):   
+    rclpy.init(args=args)
+    image_subscriber = ImageSubcriber()
+    rclpy.spin(image_subscriber)
+    image_subscriber.destroy_node()
+    rclpy.shutdown()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()  
